6ee2e8d3724b6574950c4f0646be8de1db62de9f
Joel Cumberland <joel_c@zoho.com>
2016-02-05 01:22:01 +1100

    added hack to help stop round game objects from rolloing when idle

--- CameraControllers/3DZeldaType/Assets/Scripts/PlayerController.cs
+++ CameraControllers/3DZeldaType/Assets/Scripts/PlayerController.cs
@@ -3,8 +3,11 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
+	public bool playerIdle = false;
+
+	private float jumpPower = 0.50f;
+	private float playerAboveLevel = 0.5f;
 	private float speed;
-	private float jumpPower;
 	private Rigidbody rb3d;
 	private float horizontal;
 	private float vertical;
@@ -12,7 +15,6 @@ public class PlayerController : MonoBehaviour {
 	private Transform cam;
 	private Vector3 camForward;
 	private Vector3 moveDirection;
-	private float playerAboveLevel = 0.5f;
 
 	void Awake ()
 	{
@@ -24,11 +26,12 @@ public class PlayerController : MonoBehaviour {
 		rb3d = GetComponent<Rigidbody> ();	
 
 		//Set startup position
-		rb3d.MovePosition(new Vector3(10.0f, playerAboveLevel, 0.0f));
+		rb3d.MovePosition(new Vector3(0.0f, playerAboveLevel, 0.0f));
 	}
 
 	void LateUpdate () 
 	{
+
 		horizontal = Input.GetAxis ("Horizontal");
 		vertical = Input.GetAxis ("Vertical");
 
@@ -37,29 +40,39 @@ public class PlayerController : MonoBehaviour {
 		moveDirection = (vertical * camForward + horizontal * cam.right).normalized;
 
 		rb3d.AddForce(moveDirection * moveFaster());
+
 	}
 		
 	void Update ()
 	{
+		checkPlayerIdle ();
 		playerJump ();
+
 	}
 
 	private float moveFaster ()
 	{
-		if (Input.GetKey (KeyCode.LeftShift)) {
+		if (Input.GetKey (KeyCode.LeftShift)) 
 			speed = 10;
-		} else {
+		else
 			speed = 2;
-		}	
 		return speed;
+	}
+
+
+	//Used so rounded objects like sphere's and helps stop them from rolling when no buttons pressed (hackish)
+	private void checkPlayerIdle()
+	{
+		if (!Input.GetKey (KeyCode.W) || !Input.GetKey (KeyCode.A)
+		    || !Input.GetKey (KeyCode.S) || !Input.GetKey (KeyCode.D)
+		    || !Input.GetKey (KeyCode.Space))
+			rb3d.Sleep();
 	}
 
 	private void playerJump ()
 	{
-		if (Input.GetKey (KeyCode.Space)) {
-			jumpPower = 0.5f;
+		if (Input.GetKeyDown (KeyCode.Space)) 
 			rb3d.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
-
-		}
+		
 	}
 }
